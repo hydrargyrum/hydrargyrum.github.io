3cf6591bfe5eacdc62f40f91b438e3981688baeb
hydrargyrum <dev@indigo.re>
2020-04-12 20:16:10 +0200

    update-lastmod: support taking direct filename

--- update-lastmod.sh
+++ update-lastmod.sh
@@ -4,17 +4,26 @@
 # this is useful for jekyll-sitemap plugin
 # https://github.com/jekyll/jekyll-sitemap#lastmod-tag
 
-git ls-files '*.md' | while read f
-do
-	grep -q '^---$' "$f" || continue  # no front matter: not a jekyll file
+update_file_meta () {
+	grep -q '^---$' "$1" || continue  # no front matter: not a jekyll file
 
-	dt=$(git log -1 --grep last_modified_at --invert-grep --format=%aI "$f")
+	dt=$(git log -1 --grep last_modified_at --invert-grep --format=%aI "$1")
 
-	if grep -q '^last_modified_at: ' "$f"
+	if grep -q '^last_modified_at: ' "$1"
 	then
-		sed -i "s/^last_modified_at: .*$/last_modified_at: $dt/" "$f"
+		sed -i "s/^last_modified_at: .*$/last_modified_at: $dt/" "$1"
 	else
-		sed -i "s/^title: .*$/&\nlast_modified_at: $dt/" "$f"
+		sed -i "s/^title: .*$/&\nlast_modified_at: $dt/" "$1"
 	fi
-done
+}
+
+if [ $# -eq 0 ]
+then
+	git ls-files '*.md' | while read f
+	do
+		update_file_meta "$f"
+	done
+else
+	update_file_meta "$1"
+fi
 
