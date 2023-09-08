537b1ccc74aef0dbc1d00769672b6f03e5df19b1
hydrargyrum <dev@indigo.re>
2023-09-08 10:58:23 +0200

    update-lastmod.sh: add get_date function

--- update-lastmod.sh
+++ update-lastmod.sh
@@ -5,25 +5,27 @@
 # https://github.com/jekyll/jekyll-sitemap#lastmod-tag
 
 update_file_meta () {
-	grep -q '^---$' "$1" || continue  # no front matter: not a jekyll file
-
-	dt=$(git log -1 --grep last_modified_at --invert-grep --format=%aI "$1")
+	grep -q '^---$' "$1" || return  # no front matter: not a jekyll file
 
 	if grep -q '^last_modified_at: ' "$1"
 	then
-		sed -i "s/^last_modified_at: .*$/last_modified_at: $dt/" "$1"
+		sed -i "s/^last_modified_at: .*$/last_modified_at: $2/" "$1"
 	else
-		sed -i "s/^title: .*$/&\nlast_modified_at: $dt/" "$1"
+		sed -i "s/^title: .*$/&\nlast_modified_at: $2/" "$1"
 	fi
+}
+
+get_date () {
+	git log -1 --grep last_modified_at --invert-grep --format=%aI "$1"
 }
 
 if [ $# -eq 0 ]
 then
 	git ls-files '*.md' | while read f
 	do
-		update_file_meta "$f"
+		update_file_meta "$f" "$(get_date "$f")"
 	done
 else
-	update_file_meta "$1"
+	update_file_meta "$1" "$(get_date "$1")"
 fi
 
